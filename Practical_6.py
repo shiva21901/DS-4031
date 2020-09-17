b33ba33e670514cd6ed7280124f89f511f6b14dd
shiva21901 <61749754+shiva21901@users.noreply.github.com>
2020-09-17 18:50:34 +0530

    Add files via upload

---
+++ Practical_6.py
@@ -0,0 +1,42 @@
+#NAME    : M.Shiva
+#ROLL_NO : 4031
+#CLASS   : SYCS 
+a = str(input("enter the string i for insertion sort , b for bubble sort , s for selection sort : "))
+nums = [5,4,4031,-1]
+if a=='i':
+   
+    for i  in range(1,len(nums)):
+        currentvalue = nums[i]
+        position = i
+        while(position>0 and nums[position-1] > currentvalue):
+            nums[position] = nums[position-1]
+            position = position-1
+    nums[position] = currentvalue
+    print(nums)
+elif a == 'b' :
+    
+    def sort(nums):
+        for i in range(len(nums)-1,0,-1):
+            for j in range(i):
+                if nums[j]>nums[j+1]:
+                    temp = nums[j]
+                    nums[j]=nums[j+1]
+                    nums[j+1] = temp 
+    sort(nums)
+    print(nums)
+elif a == 's':
+    def sort(nums):
+        for i in range(len(nums)):
+            minpos = i
+            for j in range(i,len(nums)):
+                if nums[j] < nums[minpos]:
+                    minpos=j
+            temp = nums[i]
+            nums[i] = nums[minpos]
+            nums[minpos] =temp
+
+    
+    sort(nums)
+    print(nums)
+else:
+    print("Enter valid input")
---
+++ practical_5.py
@@ -0,0 +1,50 @@
+#NAME    : M.Shiva
+#ROLL_NO : 4031
+#CLASS   : SYCS 
+a = str(input("Enter the string  l for Linear Search and b For Binary Search: "))
+pos = -1
+list = [0,1,2,3,4,5,6,7,8,45,31]
+if a == 'b':
+    def search(list,n):
+        l = 0
+        u = len(list)-1
+        while l <=u:
+            mid = (l+u)//2
+
+            if list[mid] == n:
+                globals()['pos']= mid
+                return True
+            else:
+                if list[mid]<n:
+                    l = mid+1
+                else:
+                    u = mid-1
+        return False            
+    #list = [0,1,2,3,4,5,6,7,8,45,31]
+    list.sort()
+    n= int(input("Enter the numbers for binary  search : "))
+    if search(list, n):
+        print("Number Found  : ")
+    else:
+        print("Not Found : ")
+elif a == 'l':
+    #pos = -1
+    def search(list ,n):
+        i = 0
+
+        while i < len(list):
+            if list[i] == n:
+                return True
+            i = i+1
+
+        return False
+
+    list.sort()
+    n= int(input("Enter the numbers for linear search : "))
+    if search(list ,n):
+        print("Number found ")
+    else:
+        print("not found")
+    
+else:
+    print("enter valid input")
